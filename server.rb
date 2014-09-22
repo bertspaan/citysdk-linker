afe726d09a36e7ba51e800a557eb27531bf568ca
Bert Spaan <bert.spaan@gmail.com>
2014-09-22 16:05:11 +0200

    simple data & osm api

--- server.rb
+++ server.rb
@@ -6,6 +6,11 @@ require 'json'
 class CitySDK_Linker < Sinatra::Base
   config = JSON.parse(File.read('./config.json'), symbolize_names: true)
 
+  DB = Sequel.connect("postgres://#{config[:db][:username]}:#{config[:db][:password]}@#{config[:db][:host]}:#{config[:db][:port]}/#{config[:db][:database]}")
+
+  Sequel.extension :pg_hstore_ops
+  DB.extension :pg_hstore
+
   before do
     content_type 'text/html; charset=utf-8'
   end
@@ -14,13 +19,32 @@ class CitySDK_Linker < Sinatra::Base
     File.read(File.join('public', 'index.html'))
   end
 
-  get '/data/1' do
-    "Geef alle GeoJSON-data uit de eerste dataset terug!"
+  get '/data/:data' do |data|
+    content_type 'application/json; charset=utf-8'
+    File.read(File.join('data', "#{data}.geojson"))
   end
 
-  get '/data/2' do
-    id = params["id"]
-    "Geef alle relevante data uit de tweede dataset terug, gerelateerd aan object #{id} uit dataset 1!"
+  get '/data/osm/:lat/:lon' do |lat, lon|
+    content_type 'application/json; charset=utf-8'
+
+    radius = 100
+    features = []
+
+    # Get all objects from view osm_objects which completely intersect with circle with radius <radius> m.
+    where = "ST_Contains(ST_Transform(Geometry(ST_Buffer(Geography(ST_Transform(ST_SetSRID(ST_Point(#{lon}, #{lat}), 4326), 4326)), #{radius})), 4326), way)"
+    dataset = DB[:osm_objects].select(:osm_id, :tags, Sequel.function(:ST_AsGeoJSON, :way).as(:geojson)).where(where).each do |row|
+      features << {
+        type: "Feature",
+        id: row[:osm_id],
+        properties: row[:tags].to_h,
+        geometry: JSON.parse(row[:geojson])
+      }
+    end
+
+    {
+      type: "FeatureCollection",
+      features: features
+    }.to_json
   end
 
 end
